ab7678a93c093c341c479e4eb0e926d06468174c
jeankory500 <124745650+jeankory500@users.noreply.github.com>
2023-06-22 01:27:21 +0000

    codeUpdate1

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,15 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 const TodoList = () => {
+
     const [ inputValue, setInputValue] = useState("");
-    const [todos, setTodos] = useState([]);
-    const [id, setid] = useState(1)
-    const handleDeleteTodo = (index) => {
-            const newTodos = [...todos];
-            newTodos.splice(index, 1);
-            setTodos(newTodos);
+
+    const inputTextHandler = (e) => {
+        setInputValue(e.target.value);
         };
+
+    const [todos, setTodos] = useState([]);
     
+    const handleDeleteTodo = (index) => {
+            const newTodos = todos.filter((item, i) => i != index);
+            setTodos(newTodos);
+                console.log(newTodos, "deleteTodo")
+            updateApi(newTodos);
+        };
+
+  
+
+    const APIURL = 'https://assets.breatheco.de/apis/fake/todos/user/jeankory500';
+
+    const getTasks = () => {
+        fetch(APIURL) 
+            .then ((response) => response.json())
+            .then ((newTodo) => setTodos(newTodo))
+        
+    }
+
+    console.log(todos)
+    
+    const submitTodoHandler = (incomingList) => {
+      //  e.preventDefault();
+        //const newTodo = [...todos, { label: inputValue, done: false }];
+        const newTodos = [...incomingList, { label: inputValue, done: false}]
+        console.log(newTodos, "inputTodo");
+        updateApi(newTodos)
+           // setInputValue("");
+            };
+    
+        const updateApi = (newTodos) => {
+            fetch(APIURL, {method: "PUT",
+            body: JSON.stringify(newTodos),
+            headers: {
+            "Content-Type": "application/json",
+            }})
+            .then((response) => {
+            response.status === 200 ? setTodos(newTodos) : ""
+            });
+        };
+
+        useEffect(() => {getTasks();}, []);
+    
+        console.log(todos)
+        
     return (
         <div className="container">
             <h1>My Todos</h1>
@@ -17,44 +62,42 @@ const TodoList = () => {
                 <li>
                     <input      
                         type="text"
-                        onChange= {(e) => setInputValue(e.target.value)}
+                        onChange={inputTextHandler}
                         value={inputValue}
-                        onKeyDown= {(e) => {
-                        console.log("key was pressed", e.key)
-                            if (e.key === "Enter") { 
-                                const item = {
-                                    string: inputValue, 
-                                    id: id
-                                }
-                                setTodos([...todos, item]);
-                                setInputValue("");
-                                setid(id => id+1);
-                                }
-                            }}
                         placeholder= "What do you need done?">
                     </input>
+                    <button type='submit' className='submitTo' onClick = {() => submitTodoHandler(todos)}>
+                        Submit
+                    </button>
                 </li>
 
-                {todos.map((item) => (
-                    <div key= {item.id}>
+
+                {todos.map((item, i) =>  {
+                console.log(item, "item") 
+                return (
+                <div key= {i}>
                         <ul>
                             <li >
-                            <span>{item.string}</span><i 
+                            <span>{item.label}</span><i 
                             className="fas fa-trash-alt"
-                            onClick={() => handleDeleteTodo([])}
+                            onClick={() => handleDeleteTodo(i)}
                             >
                             </i>
                             </li>   
                         </ul>
-                    </div>
+                    </div>)
                     
-                ))}
+                    
+})}
         
             </ul>
             <div> {todos.length} task </div>
         </div>
   )
+
 }
+
+
 //add into array --> concat
 //Delete from array --> filter 
 // update --> map
